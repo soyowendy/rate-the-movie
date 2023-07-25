d58c52ad179eae950786219b85b35e78cff22a3d
Wenderson Santos <souowendy@gmail.com>
2023-07-25 14:01:17 -0300

    Initial Commit

---
+++ backend/src/main/java/br/com/souowendy/ratethemovie/RateTheMovieApplication.java
@@ -0,0 +1,13 @@
+package br.com.souowendy.ratethemovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateTheMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateTheMovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/souowendy/ratethemovie/RateTheMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.souowendy.ratethemovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateTheMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
