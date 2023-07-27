d5b54b786e5fe3fb50f6c74500586352171b10d0
Wenderson Santos <souowendy@gmail.com>
2023-07-27 12:58:24 -0300

    Show Movies in Listing Page B)

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,15 +1,12 @@
 import { Link } from "react-router-dom";
 import MovieScore from "../MovieScore";
+import { Movie } from "../../types/movie";
 
-function MovieCard() {
-	const movie = {
-		id: 1,
-		image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
-		title: "Oppenheimer",
-		count: 3,
-		score: 4.9
-	}
+type Props = {
+	movie: Movie;
+}
 
+function MovieCard( { movie } : Props) {
 	return (
 		<div>
 			<img className="rtm-movie-card-image" src={movie.image} alt={movie.title} />
